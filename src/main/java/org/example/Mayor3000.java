8861f61e7a734157f907af13107cb37ed68061c4
Dibo72 <humanida133@gmail.com>
2026-03-20 10:29:40 +0100

    Ejercicio acabado

--- src/main/java/org/example/Mayor3000.java
+++ src/main/java/org/example/Mayor3000.java
@@ -1,17 +1,30 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Mayor3000 {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try(Connection conn = DriverManager.getConnection(
+                DBConfig.getUrl(),
+                DBConfig.getUser(),
+                DBConfig.getPassword());
+            Statement stmt = conn.createStatement()){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            String sql = "SELECT count(*) as cantidad FROM empleado WHERE salario >3000";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){
+                int cantidad = rs.getInt("cantidad");
+                System.out.println("Numero de empleados con salario mayor a 3000: " + cantidad);
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
